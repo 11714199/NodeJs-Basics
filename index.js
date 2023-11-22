160d27b504864baa6bd73ef4b44ba8c157f2401e
Madhavi Asam <madhavi.asam@teamlease.com>
2023-11-22 08:01:56 +0530

    Http Server

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const coreModules = require('./coreModules.js');
 const fileName = "fileName.txt";
+// core module
+const http = require('http');
+
 
 async function fileSystem() {
     try {
@@ -12,5 +15,43 @@ async function fileSystem() {
         console.log(err)
     }
 }
+
+let server = http.createServer((req, res) => {
+    if(req.method == "GET") {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end("Hello World")
+    } else if(req.method == "POST") {
+        res.end("POST API")
+    }
+})
+
+server.listen(1234, () => {
+    console.log("Server running at 1234")
+})
+
+// calling third party api with http module
+let options = {
+    hostname: 'www.example.com',
+    port: 80,
+    path: '/',
+    method: 'GET',
+}
+
+const req = http.request(options, (res) => {
+    let data = '';
+    res.on('data', (result) => {
+        console.log('Data:  ', result);
+    });
+
+    res.on('end', (result) => {
+        console.log("END:  ", result)
+    })
+})
+
+req.on('error', (err) => {
+    console.log("Error: ", err)
+})
+
+req.end();
 
 fileSystem();
